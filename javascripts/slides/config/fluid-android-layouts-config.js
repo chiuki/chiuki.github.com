9198703716eb0e2baf00113dba4d173c7f8cf9ce
Chiu-Ki Chan <chiuki@gmail.com>
2013-03-14 17:36:07 -0700

    Add 'Fluid Android Layout' talk

---
+++ javascripts/slides/config/fluid-android-layouts-config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Fluid Android Layouts',
+    subtitle: 'February 28, 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Chiu-Ki Chan',
+    gplus: 'http://plus.google.com/106114190915231601949',
+    twitter: 'chiuki',
+    www: 'http://blog.sqisland.com',
+    github: 'http://github.com/chiuki'
+  }]
+};
+
