20011b50a7767a436c5766fe89f3a0a5d5075c40
Chiu-Ki Chan <chiuki@gmail.com>
2013-03-14 17:16:52 -0700

    Add talk list and Beautiful Android talk

---
+++ javascripts/slides/config/beautiful-android.js
@@ -0,0 +1,9 @@
+require([
+  '../order!beautiful-android-config',
+  '../order!../modernizr.custom.45394',
+  '../order!../prettify/prettify',
+  '../order!../hammer', 
+  '../order!../slide-controller',
+  '../order!../slide-deck'], 
+  function(someModule) {
+});
