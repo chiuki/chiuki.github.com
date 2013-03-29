fb8746f21000d663060d55e23b0cd85ba66cea98
Chiu-Ki Chan <chiuki@gmail.com>
2013-03-28 22:45:00 -0700

    Add caching talks

---
+++ javascripts/slides/config/mobile-caching-strategies.js
@@ -0,0 +1,9 @@
+require([
+  '../order!mobile-caching-strategies-config', 
+  '../order!../modernizr.custom.45394',
+  '../order!../prettify/prettify',
+  '../order!../hammer', 
+  '../order!../slide-controller',
+  '../order!../slide-deck'], 
+  function(someModule) {
+});
