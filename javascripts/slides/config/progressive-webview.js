a311ae92829cba9fb1cecb0e170b37721f07b1e5
Chiu-Ki Chan <chiuki@gmail.com>
2013-04-05 16:40:11 -0700

    Add 'Progressive enhancement for Android web apps' talk

---
+++ javascripts/slides/config/progressive-webview.js
@@ -0,0 +1,9 @@
+require([
+  '../order!progressive-webview-config',
+  '../order!../modernizr.custom.45394',
+  '../order!../prettify/prettify',
+  '../order!../hammer', 
+  '../order!../slide-controller',
+  '../order!../slide-deck'], 
+  function(someModule) {
+});
